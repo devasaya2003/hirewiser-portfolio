cf1503960304e931270707a7eea58328e00e47d6
Devasaya Singh <devasyasingh2000@gmail.com>
2025-10-06 01:07:04 +0530

    added missing dependecy

--- src/components/portfolio-content.tsx
+++ src/components/portfolio-content.tsx
@@ -27,7 +27,7 @@ export function PortfolioContent({ portfolioData }: { portfolioData: PortfolioDa
 
   useEffect(() => {
     capturePostHogEvent(portfolioData.username);
-  },[]);
+  },[portfolioData.username]);
 
   return (
     <main className="flex flex-col min-h-[100dvh] space-y-6 md:space-y-8 px-4  md:pt-8 pb-20 md:pb-10">
